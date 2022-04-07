e6bf3fec6f5720faea918b3dc6e9e6413f83e857
KateDen <denisova.ea@phystech.edu>
2022-04-07 15:53:55 +0300

    Update 5-2.py

--- 5-2.py
+++ 5-2.py
@@ -1,47 +1,43 @@
 import RPi.GPIO as GPIO
 import time
 
+GPIO.setmode(GPIO.BCM)
+
 dac = [26, 19, 13, 6, 5, 11, 9, 10]
-res = [0, 0, 0, 0, 0, 0, 0, 0]
+cad_reverse = [10, 9, 11, 5, 6, 13 , 19, 26]
+array = [0, 0 ,0 ,0 ,0 ,0 ,0 ,0]
 comp = 4
 troyka = 17
-
-GPIO.setmode(GPIO.BCM)
 
 GPIO.setup (dac, GPIO.OUT)
 GPIO.setup (troyka, GPIO.OUT, initial = GPIO.HIGH)
 GPIO.setup (comp, GPIO.IN)
-
 def dec2bin(n):
     return [int(i) for i in bin(n)[2:].zfill(8)]
 
-def bin2dec(res):
-    for i in range (8):
-        num = res[i-1]*2^(i-1)
-    return num
 
+def adc():
+    for i in 7, 6, 5, 4, 3, 2, 1, 0:
+        GPIO.output (cad_reverse[i], 1)
+        array[7 - i] = 1
+        time.sleep(0.007)
 
-def adc ():
-    while (True):
-        for i in [0, 1, 2, 3, 4, 5, 6, 7]:
-            GPIO.output(dac[i], 1)
-            time.sleep (0.01)
-            if GPIO.input(comp) == 0:
-                GPIO.output(dac[i], 1)
-                res[i] = 1
-            else:
-                res[i] = 0
-        return bin2dec(res)
+        if (GPIO.input(comp) == 0):
+            GPIO.output (cad_reverse[i], 0)
+            array[7 - i] = 0
 
-
+    return i
 
 try:
-    while(True):
-        n = adc()
-        print ('Цифровое = ', res, 'An = ', n)
-        GPIO.output(dac, 0)
-
-
-finally: 
+    while (1):
+        v = 0
+        array = [0, 0 ,0 ,0 ,0 ,0 ,0 ,0]
+        GPIO.output(dac, array)
+        adc()
+        for j in range(8):
+            v += array[j]*(2**(7-j))
+            volt = v * 3.3 / 256
+        print(dec2bin(v), v, " ", volt)
+finally:
     GPIO.output(dac, 0)
     GPIO.cleanup()
